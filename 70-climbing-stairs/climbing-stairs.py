64329deb9adc311a56befa249664cf2c39fc328c
Maithreyee Mysore Sampath Kumar <mmysores@andrew.cmu.edu>
2025-12-22 13:28:01 -0500

    Time: 0 ms (100.00%) | Memory: 17.1 MB (99.34%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,13 @@
+#1d array:  memoization
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [-1]*(n+1) #declare the array
+        dp[0] = 1
+        dp[1]=1
+
+        for i in range(2,n+1):
+            dp[i] = dp[i-1]+dp[i-2] #store the answer
+        
+        return dp[n]
+
+#TC : O(N), SC = O(N) : No recursion stack used.
