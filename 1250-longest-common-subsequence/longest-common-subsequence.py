6f0acdfa9cc06abedb8f0c5da79eb13c472eef92
Maithreyee Mysore Sampath Kumar <mmysores@andrew.cmu.edu>
2026-01-25 14:49:24 -0500

    Time: 591 ms (32.52%) | Memory: 27.8 MB (83.65%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -19,21 +19,25 @@ class Solution:
         
         index1 = len(text1)
         index2 = len(text2)
-        def f(i,j,text1,text2,dp):
 
-            if i<0 or j<0:
-                return 0
+        dp = [[-1]*(index2+1) for i in range(index1+1)]
+
+        def f(i,j,text1,text2,dp):
 
             if dp[i][j] != -1:
                 return dp[i][j]
+            if i==0 or j==0:
+                return 0
 
-            elif text1[i] == text2[j]:
-                dp[i][j]= 1+f(i-1,j-1,text1,text2,dp)
+            elif text1[i-1] == text2[j-1]:
+                dp[i][j]= 1 +f (i-1,j-1,text1,text2,dp)
                 return dp[i][j]
             
             else:
                 dp[i][j]= max(f(i-1,j,text1,text2,dp),f(i,j-1,text1,text2,dp))
                 return dp[i][j]
-        dp = [[-1]*index2 for i in range(index1)]
-        return f(index1-1,index2-1,text1,text2,dp)
+        
+        
+        
+        return f(index1,index2,text1,text2,dp)
     
