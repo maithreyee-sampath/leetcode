3dc9556bd385f748d6f20a2ddb57a40d8fdf5953
Maithreyee Mysore Sampath Kumar <mmysores@andrew.cmu.edu>
2026-01-25 14:59:20 -0500

    Time: 402 ms (72.61%) | Memory: 44.5 MB (35.88%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,43 +1,18 @@
 class Solution:
-    # def f(self,i,j,text1,text2,dp):
-
-    #         if i<0 or j<0:
-    #             return 0
-
-    #         if dp[i][j] != -1:
-    #             return dp[i][j]
-
-    #         elif text1[i] == text2[j]:
-    #             dp[i][j]= 1+self.f(i-1,j-1,text1,text2,dp)
-    #             return dp[i][j]
-            
-    #         else:
-    #             dp[i][j]= max(self.f(i-1,j,text1,text2,dp),self.f(i,j-1,text1,text2,dp))
-    #             return dp[i][j]
-        
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        
-        index1 = len(text1)
-        index2 = len(text2)
+        m = len(text1)
+        n = len(text2)
 
-        dp = [[-1]*(index2+1) for i in range(index1+1)]
+        dp = [[None] * (n+1) for i in range(m+1)] #an extra space for if there is no match
 
-        def f(i,j,text1,text2,dp):
+        for i in range(m+1):
+            for j in range(n+1):
+                if i==0 or j ==0:
+                    dp[i][j] = 0
 
-            if dp[i][j] != -1:
-                return dp[i][j]
-            if i==0 or j==0:
-                return 0
+                elif text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1+ dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
 
-            elif text1[i-1] == text2[j-1]:
-                dp[i][j]= 1 +f (i-1,j-1,text1,text2,dp)
-                return dp[i][j]
-            
-            else:
-                dp[i][j]= max(f(i-1,j,text1,text2,dp),f(i,j-1,text1,text2,dp))
-                return dp[i][j]
-        
-        
-        
-        return f(index1,index2,text1,text2,dp)
-    
+        return dp[m][n]
