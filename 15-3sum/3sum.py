13be17378f75b63e225d548c2c7d9c97a0763301
Maithreyee Mysore Sampath Kumar <maithreyeesampath@gmail.com>
2026-06-24 23:10:00 -0400

    Time: 615 ms (68.01%) | Memory: 22.2 MB (81.12%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,33 +1,27 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        n = len(nums)
-        res = []
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        #[-1,0,1,2,-1,-4]
+
+        res = [] #empty res list   [-4,-1,-1,0,1,2]
+        nums.sort() #sort it        i  l       r
+
         for i in range(len(nums)):
-            if i > 0 and nums[i]== nums[i-1] :
+            if i !=0 and nums[i] == nums[i-1]:  # edge case: where i is checked for duplicates
                 continue
-            left = i+1
-            right = n-1
             
-            while left < right:
-                curr_sum = nums[i] + nums[left]+nums[right]
-                if curr_sum ==0:
-                    res.append([nums[i],nums[left],nums[right]])
-                    left +=1
-                    right-=1
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    # skip duplicate right values
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-                elif curr_sum < 0:
-                    left +=1
-                elif curr_sum > 0:
-                    right-=1
+            # 2 sum on the remainder of list
+            l = i+1
+            r = len(nums)-1
+
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+                if total > 0:
+                    r -=1
+                elif total < 0:
+                    l +=1
+                else:
+                    res.append([nums[i], nums[l], nums[r]]) #appended res if sum is 0
+                    l+=1
+                    while l < r and nums[l] == nums[l-1]:
+                        l +=1
         return res
-
-
-
-    
-
-
